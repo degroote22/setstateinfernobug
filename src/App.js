3da196a76688835150527e9e2fb9d470c4f3ac28
degroote <lucaxx@gmail.com>
2017-02-14 21:14:32 -0200

    using rheostat

--- src/App.js
+++ src/App.js
@@ -1,77 +1,35 @@
 import Inferno from "inferno";
 import Component from "inferno-component";
-
-class TestComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value
-    };
-  }
-
-  fireChange = caller => () => {
-    const value = this.state.value;
-    console.log(
-      "Firing change. It was called by the callback of setState on updateValues where we set value to 50. It should be 50 but it is:",
-      value
-    );
-    this.props.onChange(value);
-  };
-
-  updateValues = value => {
-    console.log("About to update value to", value, "and call fireChange");
-    this.setState(
-      {
-        value
-      },
-      this.fireChange()
-    );
-  };
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.value !== this.props.value) {
-      this.updateValues(nextProps.value);
-    }
-  }
-
-  render() {
-    return <div>{this.props.value}</div>;
-  }
-}
+import Rheostat from "rheostat";
 
 class App extends Component {
   state = {
     value: 9
   };
 
-  logNewValue = caller => () => {
-    console.log(
-      "After " + caller + " the parent sees its state as",
-      this.state.value
-    );
-  };
   componentDidMount() {
+    this.setState({
+      value: 50
+    });
+    //or
     setTimeout(
-      () => this.setState(
-        {
-          value: 50
-        },
-        this.logNewValue("componentDidMount timeout")
-      ),
-      300
+      () => this.setState({
+        value: 50
+      }),
+      500
     );
   }
 
-  onChange = value => {
-    this.setState({ value }, this.logNewValue("onChange"));
+  onChange = evt => {
+    this.setState({ value: evt.values[0] });
   };
 
   render() {
     const value = this.state.value;
-    console.log("Rendering:", value);
     return (
       <div>
-        <TestComponent value={value} onChange={this.onChange} />
+        Should be 50, but it is: {this.state.value}
+        <Rheostat min={0} max={100} values={[value]} onChange={this.onChange} />
       </div>
     );
   }
